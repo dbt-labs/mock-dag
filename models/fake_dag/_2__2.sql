96fb131b5245a1a093437f7226ed534b9ca19dc5
Pat Kearns <pat.kearns@dbtlabs.com>
2023-01-30 05:47:39 +0000

    remove models

--- models/fake_dag/_2__2.sql
+++
@@ -1,9 +0,0 @@
-select * from {{ ref('_1__2') }} 
-  union all 
-select * from {{ ref('_1__3') }} 
-  union all 
-select * from {{ ref('_1__4') }} 
-  union all 
-select * from {{ ref('_0__10') }} 
-  union all 
-select 1 as dummmy_column_1 
